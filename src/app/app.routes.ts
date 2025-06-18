1f760bf496539b638183ced3c421dd44476db29f
Adriann Postigo <adriannparanhos@gmail.com>
2025-06-18 19:26:07 -0300

    Init: init: initial project structure with tabs layout and basic services
    - Created Ionic/Angular project with tabs layout
    - Generated pages and services: pokemon-details, pokemon.service, favorites.service
    - Installed Ionic Storage for managing favorites

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'pokemon-details',
+    loadComponent: () => import('./pages/pokemon-details/pokemon-details.page').then( m => m.PokemonDetailsPage)
+  },
 ];
